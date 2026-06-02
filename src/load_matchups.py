bc479c3050beead35276b4b55d5e0c4c8ffc37d3
João Gabriel Nóbrega <joaog.nobrega01@gmail.com>
2026-06-02 00:06:00 -0300

    Initial project setup and matchup loader

---
+++ src/load_matchups.py
@@ -0,0 +1,47 @@
+# src/load_matchups.py
+
+import pandas as pd
+
+FILE_PATH = "data/raw/round_of_32_predictions.txt"
+
+
+def load_matchups(file_path):
+
+    matchups = []
+
+    match_id = 0
+
+    stage = "Round of 32"
+
+    with open(file_path, "r", encoding="utf-8") as file:
+
+        for line in file:
+
+            line = line.strip()
+
+            if not line:
+                continue
+
+            team_a, team_b = line.split(" x ")
+
+            matchups.append(
+                {
+                    "match_id": match_id,
+                    "stage": stage,
+                    "team_a": team_a,
+                    "team_b": team_b
+                }
+            )
+
+            match_id +=1
+
+    return matchups
+
+
+matchups = load_matchups(FILE_PATH)
+
+print(f"Total matchups loaded: {len(matchups)}")
+
+df_matchups = pd.DataFrame(matchups)
+
+print(df_matchups.head())
